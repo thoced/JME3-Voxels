48ec9411dc8bb0bb4a59cce294d657e10808df58
Thonon <Thonon@192.168.1.4>
2016-12-26 20:27:52 +0100

    Ajout du SubVoxel + Fog

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -3,6 +3,8 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
@@ -20,6 +22,9 @@ import com.jme3.math.Ray;
 import com.jme3.math.Transform;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
+import com.jme3.post.filters.FogFilter;
+import com.jme3.post.filters.LightScatteringFilter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
@@ -61,25 +66,33 @@ public class Main extends SimpleApplication implements ActionListener{
     @Override
     public void simpleInitApp() {
        
-        // instan
-        
+        // instance
         this.getFlyByCamera().setMoveSpeed(64.0f);
-        
-        // creation du AppStateVoxel
+        // distance de vue
+        this.getCamera().setFrustumFar(256);
+        this.guiViewPort.setBackgroundColor(new ColorRGBA(0.9f, 0.9f, 0.9f, 1.0f));
+        // Fog
+         /** Add fog to a scene */
+        FilterPostProcessor fpp=new FilterPostProcessor(assetManager);
+        FogFilter fog = new FogFilter();
+        fog.setFogColor(new ColorRGBA(0.9f, 0.9f, 0.9f, 1.0f));
+        fog.setFogDistance(212);
+        fog.setFogDensity(1.8f);
+        fpp.addFilter(fog);
+        viewPort.addProcessor(fpp);
+
+       // creation du AppStateVoxel
         _voxelAppState = new VoxelAppState();
         this.stateManager.attach(_voxelAppState);
             
-        
-      
-       
        // Light
        AmbientLight ambientLight = new AmbientLight();
        ambientLight.setColor(new ColorRGBA((1f/255f)*157f,1f,(1f/255f)*242f,1f));
        rootNode.addLight(ambientLight);
        
        directionalLight = new DirectionalLight();
-       directionalLight.setColor(new ColorRGBA(1,1,1,1));
-       directionalLight.setDirection(new Vector3f(0.5f,-0.5f,-0.5f).normalizeLocal());
+       directionalLight.setColor(new ColorRGBA(1,0.5f,0.5f,1));
+       directionalLight.setDirection(new Vector3f(0.5f,-0.8f,-0.3f).normalizeLocal());
        rootNode.addLight(directionalLight);
   
        // initKeys
@@ -90,17 +103,21 @@ public class Main extends SimpleApplication implements ActionListener{
        _offsetVoxel = new Vector3f(0.5f,0.5f,0.5f);
        // initMark
        this.initMark();
-     
-         
+ 
     }
     
      private void initKeys()
      {
                  
         inputManager.addMapping("ADD_VOXEL",
-          new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
-          new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
+          new KeyTrigger(KeyInput.KEY_SPACE),
+          new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); 
+        
+        inputManager.addMapping("SUB_VOXEL", 
+                new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
+        
         inputManager.addListener(this, "ADD_VOXEL");
+        inputManager.addListener(this, "SUB_VOXEL");
      }
      
      private void initMark()
@@ -141,25 +158,36 @@ public class Main extends SimpleApplication implements ActionListener{
     @Override
     public void onAction(String name, boolean isPressed, float tpf) 
     {
-        if(isPressed && name.equals("ADD_VOXEL"))
+        if(isPressed)
         {
             
-            // creation du collisionresult
-            CollisionResults results = new CollisionResults();
-            // creation du rayon
-            Ray r = new Ray(this.getCamera().getLocation(),this.getCamera().getDirection());
-            _voxelAppState.getNodeVoxelChunk().collideWith(r, results);
-            
-            if(results.size() > 0)
-            {
-                // il y a une collision
-                CollisionResult closest = results.getClosestCollision();
-                // on récupère lecontact point
-                Vector3f voxelPos = closest.getContactPoint().add(closest.getContactNormal().divide(2));
-                _voxelAppState.addVoxelToGrid(voxelPos.add(_offsetVoxel)); // offsetVoxel est égale au 0.5f de décallage
-            }
-            
-            
+                // creation du collisionresult
+                CollisionResults results = new CollisionResults();
+                // creation du rayon
+                Ray r = new Ray(this.getCamera().getLocation(),this.getCamera().getDirection());
+                _voxelAppState.getNodeVoxelChunk().collideWith(r, results);
+
+                if(results.size() > 0)
+                {
+                    // il y a une collision
+                    CollisionResult closest = results.getClosestCollision();
+                    
+                    
+                    if(name.equals("ADD_VOXEL"))
+                    {
+                        // on récupère lecontact point
+                        Vector3f voxelPos = closest.getContactPoint().add(closest.getContactNormal().divide(2));
+                        _voxelAppState.addVoxelToGrid(voxelPos.add(_offsetVoxel)); // offsetVoxel est égale au 0.5f de décallage
+                    }
+                    else
+                    {
+                        // position du voxel a supprimer
+                          Vector3f voxelPos = closest.getContactPoint().subtract(closest.getContactNormal().divide(2));
+                         _voxelAppState.subVoxelToGrid(voxelPos.add(_offsetVoxel)); // offsetVoxel est égale au 0.5f de décallage
+                    }
+                        
+                }
+ 
         }
     }
 }
