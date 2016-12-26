23aedf59200fdf5540e9b6d6f7c45d31d7c3d411
Thonon <Thonon@192.168.1.4>
2016-12-26 19:07:30 +0100

    Creation des ActionListener pour les test d'ajout - Ray et Collision

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,7 +1,14 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
 import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
@@ -9,6 +16,7 @@ import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
+import com.jme3.math.Ray;
 import com.jme3.math.Transform;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
@@ -18,6 +26,7 @@ import com.jme3.scene.Mesh;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.VertexBuffer.Type;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
 import com.jme3.util.BufferUtils;
 import java.awt.Color;
 import java.util.ArrayList;
@@ -30,7 +39,7 @@ import jme3tools.optimize.GeometryBatchFactory;
  * Move your Logic into AppStates or Controls
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication implements ActionListener{
 
     private VoxelAppState _voxelAppState;
     
@@ -38,6 +47,11 @@ public class Main extends SimpleApplication {
     private Vector3f[] dirLight;
     
     private double angle = 0f;
+    
+    private Ray _rayView;
+    private CollisionResults _viewCollisionResults;
+    private Geometry _mark;
+    private Vector3f _offsetVoxel;
     
     public static void main(String[] args) {
         Main app = new Main();
@@ -68,14 +82,53 @@ public class Main extends SimpleApplication {
        directionalLight.setDirection(new Vector3f(0.5f,-0.5f,-0.5f).normalizeLocal());
        rootNode.addLight(directionalLight);
   
+       // initKeys
+       this.initKeys();
+       // ray view
+       _rayView = new Ray();
+       _viewCollisionResults= new CollisionResults();
+       _offsetVoxel = new Vector3f(0.5f,0.5f,0.5f);
+       // initMark
+       this.initMark();
      
          
     }
+    
+     private void initKeys()
+     {
+                 
+        inputManager.addMapping("ADD_VOXEL",
+          new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
+          new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
+        inputManager.addListener(this, "ADD_VOXEL");
+     }
+     
+     private void initMark()
+     {
+        Sphere sphere = new Sphere(30, 30, 0.2f);
+        _mark = new Geometry("BOOM!", sphere);
+        Material mark_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mark_mat.setColor("Color", ColorRGBA.Red);
+        _mark.setMaterial(mark_mat);
+        rootNode.attachChild(_mark);
+       
+     }
 
     @Override
     public void simpleUpdate(float tpf)
     {
+          _rayView.setOrigin(this.getCamera().getLocation());
+          _rayView.setDirection(this.getCamera().getDirection());
           
+          _viewCollisionResults.clear();
+          
+          _voxelAppState.getNodeVoxelChunk().collideWith(_rayView, _viewCollisionResults);
+          if(_viewCollisionResults.size() > 0)
+          {
+             CollisionResult result =  _viewCollisionResults.getClosestCollision();
+             _mark.setLocalTranslation(result.getContactPoint());
+             
+          }
               
    
     }
@@ -83,5 +136,30 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) 
+    {
+        if(isPressed && name.equals("ADD_VOXEL"))
+        {
+            
+            // creation du collisionresult
+            CollisionResults results = new CollisionResults();
+            // creation du rayon
+            Ray r = new Ray(this.getCamera().getLocation(),this.getCamera().getDirection());
+            _voxelAppState.getNodeVoxelChunk().collideWith(r, results);
+            
+            if(results.size() > 0)
+            {
+                // il y a une collision
+                CollisionResult closest = results.getClosestCollision();
+                // on récupère lecontact point
+                Vector3f voxelPos = closest.getContactPoint().add(closest.getContactNormal().divide(2));
+                _voxelAppState.addVoxelToGrid(voxelPos.add(_offsetVoxel)); // offsetVoxel est égale au 0.5f de décallage
+            }
+            
+            
+        }
     }
 }
