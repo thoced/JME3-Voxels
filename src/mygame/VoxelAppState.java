678b946a90f6afe47b41fbd76d2cb54b6d1f9d41
Thonon <Thonon@192.168.1.4>
2016-12-27 17:43:03 +0100

    Creation de la methode addLightProbe

--- src/mygame/VoxelAppState.java
+++ src/mygame/VoxelAppState.java
@@ -161,6 +161,11 @@ public class VoxelAppState extends AbstractAppState {
             }
         }
     }
+    
+    public void addLightProbe(Vector3f p)
+    {
+        
+    }
 
     public Node getNodeVoxelChunk() {
         return _nodeVoxelChunk;
