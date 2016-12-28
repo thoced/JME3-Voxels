60affb15524c777ad14633edd71d98165de49110
Thonon <Thonon@192.168.1.4>
2016-12-28 11:55:13 +0100

    prepareIllumination update

--- src/mygame/LightProbe.java
+++ src/mygame/LightProbe.java
@@ -22,72 +22,77 @@ public class LightProbe
 {
     private Vector3f _position;
     
-    private float    _radius = 10f;
+    private Vector3f _positionNormalized;
+    
+    private float    _radius = 2f;
 
-    public LightProbe(Vector3f position,float radius) {
+    public LightProbe(Vector3f position,float radius) 
+    {
         this._position = position;
+        this._positionNormalized = this._position.normalize();
         this._radius = radius;
     }
 
     public void prepareIllumination(Node node)
     {
-        // d'abord on récupère tous les nodes (chunks) qui peuvent être englobé par l'illumination
-        BoundingSphere boundingSphere = new BoundingSphere(_radius,_position);
-        
-        CollisionResults results = new CollisionResults();
-       
-        
-       
+        // creation du rayon
+        Ray ray = new Ray();
+        // creatin du boundingsphere
+        BoundingSphere bSphere = new BoundingSphere(_radius,_position);
+        //pour chaque node enfants
         for(Spatial s : node.getChildren())
         {
-            s.getWorldBound().collideWith(boundingSphere, results);
-        
-        // results contient tous les node collisionnés
-        // pour chaque node collisionné, 
-        CollisionResults resultsCollisions = new CollisionResults();
-        
-        for(CollisionResult r : results)
-        {
-            System.out.println("01");
-           // pour chaque geometry, on récupère le chunk
-           if(r.getGeometry() != null)
-           {
-           Chunk c = r.getGeometry().getUserData("CHUNK");
-           // pour chaque chunk, on récupère le positino buffer
-           Vector3f[] pb = c.getPositionBuffer();
-           ColorRGBA[] cb = c.getColorBuffer();
-           // pour chaque pb, on regarde si il se trouve dans le boudingbox
-           
-           System.out.println(pb.length);
-           for(int i=0;i<pb.length;i++)
-           {
-               
-               if(boundingSphere.contains(pb[i]))
-               {
-                   // si le point est dans les phere, on lance un rayon vers la position du lightprobe pour si il n'y a pas de collision
-                   Ray ray = new Ray(pb[i],_position.subtract(pb[i]).normalizeLocal());
-                   resultsCollisions.clear();
-                   node.collideWith(ray, resultsCollisions);
-                   if(resultsCollisions.size() > 0)
-                   {
-                       // il y a une collision donc pas de modification de la lumière
-                       continue;
-                   }
-                   else
-                   {
-                      // il n'y a pas de collision donc le vertexColor est influencé par la lumière
-                      cb[i].add(ColorRGBA.White);
-                   }
-               }
-           }
-           
-           
-           //update du mesh
-           c.updateLightProbeColor();
-           }
-           
+            // si le node est intersect avec la boundingsphere
+            if(s.getWorldBound().intersectsSphere(bSphere))
+            {
+                //récupération des position vertex
+                Chunk c = s.getUserData("CHUNK");
+                Vector3f[] pb = c.getPositionBuffer();
+                Vector3f[] nb = c.getNormalBuffer();
+                ColorRGBA[] cb = c.getColorBuffer();
+                
+                // pour chaque vertex position dans le boundingsphere
+                for(int i=0;i<pb.length;i++)
+                {
+                    // si il est contenu dans le boundingSphere
+                    if(bSphere.contains(pb[i]))
+                    {
+                       // Vector3f dir = pb[i].subtract(_position);
+                        Vector3f dir =_position.subtract(pb[i]);
+                        dir.normalizeLocal();
+                        // test dot product 
+                        float dot = nb[i].dot(dir);
+                        if(dot <= 0f || dot >=1f)
+                            continue;
+                        
+                        // on lance un rayon entre le pb et la position du lightprobe
+                          
+                        ray.setOrigin(pb[i]);
+                        ray.setDirection(dir);
+                        CollisionResults results = new CollisionResults();
+                        node.collideWith(ray, results);
+                        
+                        if(results.size() > 0 && !results.getClosestCollision().getContactPoint().equals(pb[i]))
+                        {
+                            // il y a une collision, on applique pas de lumière
+                        }
+                        else
+                        {
+                            // il n'y a pas de collision, on applique de la lumière
+                            cb[i].addLocal(ColorRGBA.White);
+                            
+                            
+                        }
+                        
+                    }
+                }
+                
+                c.updateLightProbeColor(cb);
+            }
         }
-        }
+        
+         
+        
        
     }
     
