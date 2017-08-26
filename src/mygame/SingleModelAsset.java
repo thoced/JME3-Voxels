5dff799ca31a6ebf9d58e4dd82d2ef0f9f7313e2
Thonon <Thonon@192.168.1.4>
2017-08-26 23:46:36 +0200

    Class Singleton qui fourni les Assets

---
+++ src/mygame/SingleModelAsset.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import java.util.HashMap;
+
+/**
+ *
+ * @author thonon
+ */
+public class SingleModelAsset
+{
+    private static SingleModelAsset _instance;
+
+    private HashMap<String,Spatial> listAsset = new HashMap();
+    
+    private SingleModelAsset(){
+       
+    }
+
+    public HashMap<String, Spatial> getListAsset() {
+        return listAsset;
+    }
+    
+        
+    public static SingleModelAsset getInstance(){
+        if(_instance != null)
+            return _instance;
+        
+        synchronized(SingleModelAsset.class){
+        _instance = new SingleModelAsset();
+        }
+        
+        return _instance;
+    } 
+}
