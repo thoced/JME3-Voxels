3d857090ebd0de2fc1081b68103b82ce288f7f93
Thonon <Thonon@192.168.1.4>
2017-08-26 23:45:42 +0200

    Class Model Tree

---
+++ src/mygame/TreeModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author thonon
+ */
+public class TreeModel {
+
+       public int x,y,h;
+    
+    public TreeModel(int x, int y, int h) {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+    }
+
+    
+}
