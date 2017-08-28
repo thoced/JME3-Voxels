a8c5bd1de88b4d2b3b3707b5fa104655d1bbf3bc
Thonon <Thonon@192.168.1.4>
2017-08-28 17:37:59 +0200

    commit

--- src/mygame/VoxelAppState.java
+++ src/mygame/VoxelAppState.java
@@ -182,7 +182,7 @@ public class VoxelAppState extends AbstractAppState {
     public void addVoxelToGrid(Vector3f p)
     {
         // ajout du voxel dans la grid3d generale
-        _map.getGridMap3d()[((int)p.y*_map.getzWidth()) + ((int)p.z * _map.getHeightMap()) + (int) p.x] = 1;
+        _map.getGridMap3d()[((int)p.y*_map.getzWidth()) + ((int)p.z * _map.getHeightMap()) + (int) p.x] = 3;
         // on détermine le chunk qui correspond à la modification pour réinitialiser le mesh
         Vector2f chunkPos = new Vector2f((int)p.x / 16,(int)p.z / 16);
         // appel à la methode update du chunk
