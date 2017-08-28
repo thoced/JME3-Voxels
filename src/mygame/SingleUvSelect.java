7d1b19830455deaa5160d9537d58ce28fde23a84
Thonon <Thonon@192.168.1.4>
2017-08-28 17:37:22 +0200

    update du SingleUvSelect pour pouvoir selectionné le bloc terre

--- src/mygame/SingleUvSelect.java
+++ src/mygame/SingleUvSelect.java
@@ -6,6 +6,7 @@
 package mygame;
 
 import ModelsBloc.BBrick;
+import ModelsBloc.BEarth;
 import ModelsBloc.BGrass;
 import ModelsBloc.BlockBase;
 import com.jme3.math.Vector2f;
@@ -20,12 +21,14 @@ public class SingleUvSelect {
    
     private BBrick brick;
     private BGrass grass;
+    private BEarth earth;
    
     
     private SingleUvSelect(){
         
         brick = new BBrick();
         grass = new BGrass();
+        earth = new BEarth();
         
         
     }
@@ -33,6 +36,9 @@ public class SingleUvSelect {
     public BlockBase getUv(String nameCube){
 
         switch(nameCube){
+            
+            case "EARTH": return earth;
+            
             case "BRICK": return brick;
             
             case "GRASS": return grass;
